dcad85e87f5af1b34ad58c771c53436a3d44ebc4
alextaoultsides <taoofalex@yahoo.com>
2014-08-06 19:34:20 -0400

    Update eubotlawgenerator.py

--- eubotlawgenerator.py
+++ eubotlawgenerator.py
@@ -33,35 +33,34 @@ def eu_generator(cfdist, word, num= 15):
     print sent
      
     return sent
+    
 
+text = open('english.txt', 'r') #Opens European Union corpus
+texts = text.read() 
+tokens = nltk.word_tokenize(texts)#tokenizes the entire text.
+bigrams = nltk.bigrams(tokens)  #creates unique bigrams(every two words in sequence)
 
-text = open('english.txt', 'r')
-texts = text.read()
-tokens = nltk.word_tokenize(texts)
-bigrams = nltk.bigrams(tokens)
-
-cfd = nltk.ConditionalFreqDist(bigrams)
-
+cfd = nltk.ConditionalFreqDist(bigrams)#gets the frequency of each bigram in the text
 
 rando = ""
-for i in range(1):
+for i in range(1):#Can change to infinite or how many sentences and tweets you would like
     
     rando = random.choice(tokens)
     
-    print nltk.pos_tag(nltk.word_tokenize(rando))[0][0]
-    while nltk.pos_tag(nltk.word_tokenize(rando))[0][1] not in safe_tags:
+    while nltk.pos_tag(nltk.word_tokenize(rando))[0][1] not in safe_tags:#finds random word that is in the Safe tags.
         rando = random.choice(tokens)
     
     
-    a = eu_generator(cfd, rando)
+    eubot = eu_generator(cfd, rando)
     
     if len(nltk.word_tokenize(a)) > 1:
-        print a
+        print eubot
         
         
-        #time.sleep(300)
-    
+        time.sleep(300)
+        
+        #posts to Twitter,  keys and passes are secret
         api = twitter.Api(consumer_key='nope', consumer_secret='nope', access_token_key='nope', access_token_secret='nope')
-        final_sent = "Art " + str(int(random.randrange(1,5000))) + ": " + a
-        api.PostUpdate(final_sent)
+        final_sent = "Art " + str(int(random.randrange(1,5000))) + ": " + a #random fake "Article"
+        api.PostUpdate(final_sent) #posted to Twitter
     
