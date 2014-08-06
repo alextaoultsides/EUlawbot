7380f84147d309d19bf8b8f77f1ddd6babfe678e
alextaoultsides <taoofalex@yahoo.com>
2014-08-06 19:24:11 -0400

    Update eubotlawgenerator.py

--- eubotlawgenerator.py
+++ eubotlawgenerator.py
@@ -5,25 +5,24 @@ import twitter
 import random
 import re
 import time
-perc = .3   #Top 30 percent words 
-safe_tags = ['N', 'P', 'DET', 'NP','NN']
+
+safe_tags = ['N', 'P', 'DET', 'NP','NN']#Good parts of speech to start sentences with.
+
+#in: conditional frequency distribution, random seed word, length of senctence in words.
+#out: Full sentence to be sent to Twitter.
 def eu_generator(cfdist, word, num= 15):
-    perc = .2
+    perc = .2 #Top 20 percent most frequent words
     rand = 0
-    print rand
+
+    word = word.lower() #changes all text to lowercase
+    sent = word.title() #First word starts with uppercase letter
     
-    print len(cfdist[word])
-    word = word.lower()
-    sent = word.title()
-    law = ""
-    trueword = ""
     for i in range(num):
         
         percs = perc * len(cfdist[word].keys())
-        #print perc
+        
         if percs > 1 and len(sent) < 115:
             rand = random.randrange(0,int(percs))
-            #print rand
             if len(cfdist[word].keys()[rand]):
                 if re.match("^[A-Za-z_-]*$", cfdist[word].keys()[rand]):
                     word = cfdist[word].keys()[rand]
@@ -32,10 +31,9 @@ def eu_generator(cfdist, word, num= 15):
                     word = cfdist[word].max()
                     sent = " " + sent + " " + word
     print sent
-      
-    #print sent
-    #print len(sent)    
+     
     return sent
+
 
 text = open('english.txt', 'r')
 texts = text.read()
@@ -51,7 +49,6 @@ for i in range(1):
     rando = random.choice(tokens)
     
     print nltk.pos_tag(nltk.word_tokenize(rando))[0][0]
-    #print nltk.pos_tag(tokens[rando])
     while nltk.pos_tag(nltk.word_tokenize(rando))[0][1] not in safe_tags:
         rando = random.choice(tokens)
     
