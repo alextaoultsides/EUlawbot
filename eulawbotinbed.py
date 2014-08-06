603d0293c93f0bb631ee3bb450a66d87fffa1bb5
alextaoultsides <taoofalex@yahoo.com>
2014-08-06 19:36:30 -0400

    Update eulawbotinbed.py

--- eulawbotinbed.py
+++ eulawbotinbed.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+#eulawbotinbed.py
+#retrieves tweet from EULAWBOT, adds "in bed!" to the end, posts to EULAWBOTINBED and laughter ensues.
+
 import twitter
 import time
 api = twitter.Api(consumer_key='nope', consumer_secret='nope', access_token_key='nope', access_token_secret='nope')
@@ -12,7 +15,7 @@ for j in range(1):
     #print api.VerifyCredentials()
     
     
-    results = api.GetUserTimeline(screen_name = "EUlawbot")
+    results = api.GetUserTimeline(screen_name = "EUlawbot")#Gets tweets
     
     for i in results:
         print i.GetText().encode('utf-8')
